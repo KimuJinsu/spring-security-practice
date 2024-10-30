e7cfbc4dca0004d04794497b89e8d61840653a5e
Jinsu <jinsu8828@gmail.com>
2024-10-30 22:55:38 +0900

    add

--- SpringSecurityPractice/ch8_1/src/main/java/com/example/demo/controller/EmailController.java
+++ SpringSecurityPractice/ch8_1/src/main/java/com/example/demo/controller/EmailController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmailController {
+
+    @GetMapping("/email/{email}")
+    public String video(@PathVariable String email) {
+        return "Allowed for email " + email;
+    }
 
 }
